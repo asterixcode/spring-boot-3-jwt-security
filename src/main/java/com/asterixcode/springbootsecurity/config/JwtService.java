62aa25b7c0d405287df54be3c6cb3577b1a0ac55
lucas-saldanha <lk.saldanha@gmail.com>
2024-03-09 22:37:03 +0100

    generate the JWT token

--- src/main/java/com/asterixcode/springbootsecurity/config/JwtService.java
+++ src/main/java/com/asterixcode/springbootsecurity/config/JwtService.java
@@ -4,13 +4,17 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 import javax.crypto.SecretKey;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-  
+
   private static final String SECRET_KEY = "17a007917a0aebc4fa9aa0a40bef0204d5bbf8a0558311ae381f769b930c6c84";
 
   public String extractUsername(String token) {
@@ -20,6 +24,25 @@ public class JwtService {
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(new HashMap<>(), userDetails);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts.builder()
+        .claims(extraClaims)
+        // the unique identifier for the user (email in this case), but in Spring it's always called `username`
+        .subject(
+            userDetails
+                .getUsername())
+        // the time the token was issued, helping to calculate the expiration time and if the token is still valid
+        .issuedAt(new Date(System.currentTimeMillis()))
+        // expiration time of the token (1 hour in this case)
+        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // up to you to decide the value
+        .signWith(getSignInKey(), Jwts.SIG.HS256)
+        .compact();
   }
 
   private Claims extractAllClaims(String token) {
