8f2337404f26cda609e504b0bb82d8908640a4bb
lucas-saldanha <lk.saldanha@gmail.com>
2024-03-09 18:31:39 +0100

    add extract all claims and extract single claim

--- src/main/java/com/asterixcode/springbootsecurity/config/JwtService.java
+++ src/main/java/com/asterixcode/springbootsecurity/config/JwtService.java
@@ -1,12 +1,38 @@
 package com.asterixcode.springbootsecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.function.Function;
+import javax.crypto.SecretKey;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
-
+  
+  private static final String SECRET_KEY = "17a007917a0aebc4fa9aa0a40bef0204d5bbf8a0558311ae381f769b930c6c84";
 
   public String extractUsername(String token) {
     return null;
+  }
+
+  public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+    final Claims claims = extractAllClaims(token);
+    return claimsResolver.apply(claims);
+  }
+
+  private Claims extractAllClaims(String token) {
+    return Jwts
+            .parser()
+            .verifyWith(getSignInKey())
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+  }
+
+  private SecretKey getSignInKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
   }
 }
