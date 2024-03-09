04a9226ad871b00f65ddd057966d297e3f11de08
lucas-saldanha <lk.saldanha@gmail.com>
2024-03-09 22:48:13 +0100

    check if user exists in the database (JwtAuthFilter)

--- src/main/java/com/asterixcode/springbootsecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/asterixcode/springbootsecurity/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,6 +19,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
+  private final UserDetailsService userDetailsService; // from spring security core
 
   @Override
   protected void doFilterInternal(
@@ -32,6 +36,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     } else {
       jwt = authorizationHeader.substring(7);
       userEmail = jwtService.extractUsername(jwt);
+      // if email is not null and user is not authenticated, otherwise skip
+      if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+      }
     }
   }
 }
