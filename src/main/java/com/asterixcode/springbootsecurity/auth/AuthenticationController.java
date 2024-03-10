b971c48827ea6c6b8e5ed6702bba273e967d9cce
lucas-saldanha <lk.saldanha@gmail.com>
2024-03-10 11:24:06 +0100

    add register and authenticate functionality

---
+++ src/main/java/com/asterixcode/springbootsecurity/auth/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.asterixcode.springbootsecurity.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+  private final AuthenticationService authenticationService;
+
+  @PostMapping(path = "/register")
+  public ResponseEntity<AuthenticationResponse> register(
+          @RequestBody RegisterRequest request
+  ) {
+    return ResponseEntity.ok(authenticationService.register(request));
+  }
+
+  @PostMapping(path = "/authenticate")
+  public ResponseEntity<AuthenticationResponse> authenticate(
+          @RequestBody AuthenticationRequest request
+  ) {
+    return ResponseEntity.ok(authenticationService.authenticate(request));
+  }
+}
