78ec5228f09e429fdd4d751c9d91489414433f65
lucas-saldanha <lk.saldanha@gmail.com>
2024-03-10 10:46:39 +0100

    add comments to the security configuration

--- src/main/java/com/asterixcode/springbootsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/asterixcode/springbootsecurity/config/SecurityConfiguration.java
@@ -12,23 +12,40 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity // Enables Spring Security on Spring Boot 3.0+
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final AuthenticationProvider authenticationProvider;
 
+  // At application startup, Spring Security will look for a bean of type SecurityFilterChain.
+  // The bean is responsible for configuring all http security rules of the application.
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(
+    http
+            // Disable CSRF verification as we are using JWT.
+            .csrf(AbstractHttpConfigurer::disable)
+            // What are the URLs and Paths that need to be secured,
+            // and what is the white list (doesn't require any authentication or authorization token).
+            // Example of white listing: /login, /register, /forgot-password, /reset-password
+            .authorizeHttpRequests(
             authorizeRequests ->
-                authorizeRequests.requestMatchers("").permitAll().anyRequest().authenticated())
+                authorizeRequests
+                        // list of URLs that are whitelisted
+                        .requestMatchers("").permitAll()
+                        // any other URL should be authenticated
+                        .anyRequest().authenticated()
+            )
+        // Filter implement as "once per request filter" = every request should be authenticated.
+        // This means that the server doesn't store the authentication/session state = Stateless.
         .sessionManagement(
             sessionManagement ->
+                    // Set Spring to create a new session for every request.
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        // Tell Spring which authentication provider to use.
         .authenticationProvider(authenticationProvider)
+        // Add and set Spring to use the JWT filter before the UsernamePasswordAuthenticationFilter.
         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
